30c131254dafcfce7badf309dafb3174d878794a
root <root@DESKTOP-ATIM1UH.localdomain>
2020-12-21 22:37:56 +0000

    Added code to get input data from github

---
+++ mltb.py
@@ -0,0 +1,22 @@
+from pyspark import SparkFiles
+from pyspark.sql.functions import array, col, explode, struct, lit
+
+
+def get_df():
+
+	file_url = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv'
+	spark.sparkContext.addFile(file_url)
+
+
+	input_df = spark.read.csv(SparkFiles.get("time_series_covid19_confirmed_global.csv"), header=True)
+
+	input_df = input_df.withColumnRenamed('Province/State','province').withColumnRenamed('Country/Region','country')
+
+	index_columns = ['country','province', 'Lat', 'Long']
+
+	columns, dtypes = zip(*((c, t) for (c, t) in input_df.dtypes if c not in index_columns))
+
+	transpose_columns = explode(array([struct(lit(c).alias("date"), col(c).alias("cases")) for c in columns])).alias("transpose_columns")
+
+	input_df = input_df.select(index_columns + [transpose_columns]).select(index_columns + ["transpose_columns.date", "transpose_columns.cases"])
+	return input_df

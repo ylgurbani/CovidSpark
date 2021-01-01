1c743578aaecc32817acefab6ba3f87b399056d6
ylgurbani <71453387+ylgurbani@users.noreply.github.com>
2021-01-01 11:37:45 +0000

    fixed issue with province daily cases not showing correctly

--- mltb.py
+++ mltb.py
@@ -1,6 +1,9 @@
 from pyspark import SparkFiles
 from pyspark.sql.functions import array, col, explode, struct, lit
-from pyspark.sql.window import Window
+from pyspark.sql.functions import *
+import pyspark.sql.functions as f
+from pyspark.sql.types import *
+from pyspark.sql import SparkSession, SQLContext, Window
 
 def get_df():
 
@@ -23,8 +26,22 @@ def get_df():
 
 test_df = get_df()
 
-w =  Window.partitionBy(test_df.country).orderBy(test_df.country)
+test_df.createOrReplaceTempView("Pre_Dataset")
 
-daily_case_df = test_df.withColumn("diff", col("cases") - when((lag("cases", 1).over(w)).isNull(), test_df.head().cases)
-                   .otherwise(lag("cases", 1).over(w)))
-daily_case_df.show()
+df_with_null = spark.sql('SELECT *, cases - LAG(cases) OVER(PARTITION BY (CASE WHEN province IS NOT NULL THEN province \
+                                                                               ELSE country END) \
+                                                            ORDER BY (CASE WHEN province IS NOT NULL THEN province \
+                                                                           ELSE country END)) AS Null_daily_Cases \
+                          FROM Pre_Dataset')
+
+df_with_null.createOrReplaceTempView("Null_Case_DF")
+
+daily_case_df = spark.sql('SELECT *, CASE WHEN Null_Daily_cases IS NOT NULL THEN Null_Daily_Cases ELSE 0 END AS Daily_Cases \
+                           FROM Null_Case_DF')
+
+daily_case_df.createOrReplaceTempView("Daily_Case_DF")
+
+#test to see if UK province daily cases show correctly
+spark.sql('SELECT * \
+           FROM Daily_Case_DF \
+           WHERE country ="United Kingdom" AND province IS NULL').show()

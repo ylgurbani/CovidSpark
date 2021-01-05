9127392b592869d32d45584678aa47656d9f9fce
ylgurbani <71453387+ylgurbani@users.noreply.github.com>
2021-01-05 03:40:27 +0000

    added days column to continent stats

--- mltb.py
+++ mltb.py
@@ -5,7 +5,6 @@ from pyspark.sql.types import *
 from pyspark.sql import SparkSession, SQLContext, Window
 import numpy as np
 
-
 def get_df():
 	file_url = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv'
 	spark.sparkContext.addFile(file_url)
@@ -79,27 +78,25 @@ def get_stats_continents(df):
 	get_slope_udf = F.udf(get_slope, returnType=DoubleType())
 	replace_negatives_udf = F.udf(replace_negatives, returnType=ArrayType(DoubleType()))
 	shift_days_udf = F.udf(shift_days, returnType=ArrayType(IntegerType()))
+	
+	df_array_values = df.withColumn('day', dayofweek(df.date))
 	df_array_values = df.orderBy('continent','province','week','date').groupBy('continent','province','week').agg(collect_list('daily_cases').alias('daily_cases'), collect_list('day').alias('days'))
 	df_array_values = df_array_values.withColumn('daily_cases', replace_negatives_udf(F.col('daily_cases')))
 
 	df_array_values = df_array_values.withColumn('days', shift_days_udf(F.col('days')))
-
 	stats_df = df_array_values.withColumn('slope', get_slope_udf(F.col('days'), F.col('daily_cases')))
+	
 	window = Window.partitionBy( stats_df['week']).orderBy(stats_df['slope'].desc())
 	stats_df = stats_df.select('continent', 'week', 'days', 'daily_cases', rank().over(window).alias('rank')).filter(col('rank') <= 100).orderBy(stats_df.week)
-
 	stats_df = stats_df.withColumn('zip_cols', explode(arrays_zip(stats_df.daily_cases, stats_df.days))).select('continent','week','zip_cols.days','zip_cols.daily_cases')
-
 	stats_df = stats_df.groupBy('continent', 'week', 'days').agg(avg('daily_cases').alias('daily_cases'))
 	stats_df = stats_df.groupBy('continent','week').agg(avg('daily_cases').alias('average'), stddev('daily_cases').alias('deviation'), min('daily_cases').alias('minimum'), max('daily_cases').alias('maximum'))
 
 	return stats_df
 
-
 def shift_days(days):
     shifted_days = [ day - 1 if day - 1 > 0 else 7 for day in days ]
     return shifted_days
-
 
 def replace_negatives(input):
     non_negatives = [ num for num in input if num >= 0]
@@ -109,7 +106,6 @@ def replace_negatives(input):
         
     average = sum / len(non_negatives)
     return [num if num >= 0 else average for num in input]
-
 
 def cluster_top_provinces(df):
 
@@ -139,8 +135,6 @@ def cluster_top_provinces(df):
 
     return clusters
 
-
-
 def kmeans(df):
     assembler = VectorAssembler(inputCols=["slope"],outputCol="features")
     output = assembler.transform(df)
@@ -154,7 +148,6 @@ def kmeans(df):
     predictions = model.transform(output)
 
     return predictions
-
 
 df = get_df()
 df.cache()
